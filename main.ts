aa2fcd1e489c78a311242764f16fbca3fc84f863
meixnaa <47194011+meixnaa@users.noreply.github.com>
2022-08-03 13:31:47 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -15,16 +15,6 @@ enum KStop {
     Bremsen
 }
 
-enum KSensor {
-    links,
-    rechts
-}
-
-enum KSensorStatus {
-    hell,
-    dunkel
-}
-
 enum KFunk {
     an,
     aus
@@ -68,23 +58,6 @@ enum KState {
     an
 }
 
-enum KSensorWait {
-    //% block="Entfernung (cm)"
-    distanceCm,
-    //% block="Entfernung (mm)"
-    distance,
-    //% block="Helligkeit"
-    brightness,
-    //% block="Temperatur"
-    temperature,
-    //% block="Beschleunigung X"
-    accellX,
-    //% block="Beschleunigung Y"
-    accellY,
-    //% block="Beschleunigung Z"
-    accellZ
-}
-
 enum KCheck {
     //% block="="
     equal,
@@ -118,7 +91,7 @@ namespace crossroads {
             setLed(KMotor.rechts, KState.aus);
             motorStop(KMotor.beide, KStop.Bremsen);
             setRgbLed(KRgbLed.All, KRgbColor.rot, 0);
-            setTraffic(KTrafficColor.All, 0);
+            setTraffic(KTrafficMain.All, KTrafficColor.aus);
         }
     }
 
@@ -278,100 +251,4 @@ namespace crossroads {
         basic.pause(10);
     }
 
-    //="Liniensensor $sensor"
-    //% blockId K_readLineSensor color="#00C040" block="Liniensensor |%sensor| |%status"
-    export function readLineSensor(sensor: KSensor, status: KSensorStatus): boolean {
-        let result = false
-
-        let buffer = pins.i2cReadBuffer(0x21, 1);
-        KInit();
-        if (sensor == KSensor.links) {
-            buffer[0] &= 0x02
-        }
-        if (sensor == KSensor.rechts) {
-            buffer[0] &= 0x01
-        }
-        switch (status) {
-            case KSensorStatus.hell:
-                if (buffer[0] != 0) {
-                    result = true
-                }
-                else {
-                    result = false
-                }
-                break
-            case KSensorStatus.dunkel:
-                if (buffer[0] == 0) {
-                    result = true
-                }
-                else {
-                    result = false
-                }
-                break
-        }
-        return result
-    }
-
-    //% blockId=K_entfernung color="#00C040" block="Entfernung |%modus" blockGap=8
-    export function entfernung(modus: KEinheit): number {
-        let buffer = pins.i2cReadBuffer(0x21, 3)
-        KInit()
-        if (modus == KEinheit.mm) {
-            return 256 * buffer[1] + buffer[2]
-        }
-        else {
-            return (256 * buffer[1] + buffer[2]) / 10
-        }
-    }
-
-    //% blockId=K_warte color="#0082E6" block="Warte bis |%sensor| |%check| |%value"
-    export function warte(sensor: KSensorWait, check: KCheck, value: number) {
-        let abbruch = 0
-        let sensorValue = 0
-        while (abbruch == 0) {
-            switch (sensor) {
-                case KSensorWait.distance:
-                    sensorValue = entfernung(KEinheit.mm)
-                    break;
-                case KSensorWait.distanceCm:
-                    sensorValue = entfernung(KEinheit.cm)
-                    break;
-                case KSensorWait.accellX:
-                    sensorValue = input.acceleration(Dimension.X)
-                    break;
-                case KSensorWait.accellY:
-                    sensorValue = input.acceleration(Dimension.Y)
-                    break;
-                case KSensorWait.accellZ:
-                    sensorValue = input.acceleration(Dimension.Z)
-                    break;
-                case KSensorWait.brightness:
-                    sensorValue = input.lightLevel()
-                    break;
-                case KSensorWait.temperature:
-                    sensorValue = input.temperature()
-                    break;
-            }
-            switch (check) {
-                case KCheck.equal:
-                    if (sensorValue == value)
-                        abbruch = 1
-                    break;
-                case KCheck.lessThan:
-                    if (sensorValue < value)
-                        abbruch = 1
-                    break;
-                case KCheck.greaterThan:
-                    if (sensorValue > value)
-                        abbruch = 1
-                    break;
-            }
-        }
-    }
-
-    //% blockId=K_warte_LSensor color="#0082E6" block="Warte bis Liniensensor |%sensor| = |%status"
-    export function warteLSensor(sensor: KSensor, status: KSensorStatus) {
-        while (!(readLineSensor(sensor, status))) {
-        }
-    }
 }
