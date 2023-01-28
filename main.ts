57b999a3ab74633fb0112778952470f05d8b7d27
meixnaa <47194011+meixnaa@users.noreply.github.com>
2023-01-28 21:27:59 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -26,19 +26,6 @@ enum RoadSensor {
 }
 
 
-//% color="#AA278D"
-namespace CrossRoad {
-
-    //% block
-    export function Fußgängertasten(choice: PedestrianButton) {
-
-    }
-
-    //% block
-    export function Magnetsensoren(choice: RoadSensor) {
-
-    }
-}
 // Funktion zum Auslesen des Zustands einer Fußgängertaste
 // Gibt "true" zurück, wenn die Taste betätigt ist, ansonsten "false"
 function readPedestrianButton(button: PedestrianButton): boolean {
@@ -54,3 +41,95 @@ function readRoadSensor(sensor: RoadSensor): boolean {
     let sensorState = input & (1 << (sensor + 8));
     return sensorState != 0;
 }
+
+enum LedState {
+    // Enum for LED states
+    OFF,
+    RED,
+    YELLOW,
+    GREEN
+}
+
+// Define arrays to store the state of each LED
+let mainStreetLights = [LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF];
+let sideStreetLights = [LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF];
+let pedestrianLights = [LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF, LedState.OFF];
+let radarLed = LedState.OFF;
+let streetLamp = LedState.OFF;
+
+// Function to set the state of a specific LED
+function setLedState(led: number, state: LedState): void {
+    if (led < 0 || led > 29) return; // Check for invalid LED number
+    if (led < 4) {
+        mainStreetLights[led] = state;
+    } else if (led < 12) {
+        sideStreetLights[led - 4] = state;
+    } else if (led < 20) {
+        pedestrianLights[led - 12] = state;
+    } else if (led === 20) {
+        radarLed = state;
+    } else if (led === 21) {
+        streetLamp = state;
+    }
+}
+
+// Function to update the state of all LEDs
+function updateLeds(): void {
+    let mainStreetByte = 0;
+    let sideStreetByte = 0;
+    let pedestrianByte1 = 0;
+    let pedestrianByte2 = 0;
+
+    // Loop through each LED and update the corresponding byte
+    for (let i = 0; i < 4; i++) {
+        if (mainStreetLights[i] === LedState.RED) {
+            mainStreetByte += 1 << (3 - i);
+        }
+        if (sideStreetLights[i] === LedState.RED) {
+            sideStreetByte += 1 << (3 - i);
+        }
+    }
+
+    for (let i = 0; i < 8; i++) {
+        if (pedestrianLights[i] === LedState.RED) {
+            if (i < 4) {
+                pedestrianByte1 += 1 << (7 - i);
+            } else {
+                pedestrianByte2 += 1 << (7 - (i - 4));
+            }
+        }
+    }
+
+    if (radarLed === LedState.RED) {
+        sideStreetByte += 1 << 4;
+    }
+    if (streetLamp === LedState.RED) {
+        sideStreetByte += 1 << 5;
+    }
+
+    // Send the bytes to the micro:bit
+    serial.writeBuffer(pins.createBuffer(4));
+}
+
+
+//% color="#AA278D"
+namespace CrossRoad {
+
+    //% block
+    export function Fußgängertasten(choice: PedestrianButton) {
+
+    }
+
+    //% block
+    export function Magnetsensoren(choice: RoadSensor) {
+
+    }
+    
+    //% block
+    export function Ampeln(choice: LedState) {
+
+    }
+}
+
+
+
